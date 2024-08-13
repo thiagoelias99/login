339b6af08dedf608931db0179280f1f3d573169e
Thiago Elias <thiagoelias99@gmail.com>
2024-08-13 10:00:55 -0300

    fix: :bug: Add suspense

--- next_auth_v5/src/app/(root)/confirm-email/page.tsx
+++ next_auth_v5/src/app/(root)/confirm-email/page.tsx
@@ -1,84 +1,13 @@
-"use client"
-
-import { confirmToken } from '@/actions/confirm_token';
-import { getUser } from '@/actions/get_user';
-import { Loader2Icon } from 'lucide-react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import OtpInput from 'react-otp-input';
+import React, { Suspense } from 'react'
+import ConfirmEmailPageContents from './_contents'
+import { Loader2Icon } from 'lucide-react'
 
 export default function ConfirmEmailPage() {
-  const searchParams = useSearchParams()
-  const userId = searchParams.get('user')
-  const token = searchParams.get('token') || ''
-
-  const [email, setEmail] = useState('');
-  const [otp, setOtp] = useState(token);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter()
-
-  useEffect(() => {
-    if (userId) {
-      getUser(userId).then((response) => {
-        const userData = JSON.parse(response)
-        if (userData.status === 'success' && userData.user) {
-          setEmail(userData.user.email)
-        } else {
-          alert('User not found')
-        }
-        setLoading(false)
-      })
-    }
-  }, []);
-
-  async function handleConfirmToken() {
-    if (userId && otp) {
-      const result = JSON.parse(await confirmToken(userId, otp))
-      switch (result.status) {
-        case 'success':
-          router.push('/?message=email_confirmed')
-          break;
-
-        case 'invalid':
-          alert('Invalid code')
-          break;
-
-        default:
-          alert('Error')
-          break;
-      }
-    }
-  }
-
   return (
-    <div className='w-full flex flex-col justify-center items-center bg-white p-4 rounded-lg max-w-screen-sm'>
-      {loading ? (
-        <Loader2Icon size='64' className='animate-spin' />
-      ) : (
-        <div className='w-full flex flex-col justify-start items-start gap-4'>
-          <h1 className='text-lg font-bold'>Enter the 4 digits code sent to {email}</h1>
-          <div className='self-center'>
-            <OtpInput
-              value={otp}
-              onChange={setOtp}
-              numInputs={4}
-              renderSeparator={<span className='mx-2'>-</span>}
-              renderInput={(props) =>
-                <div
-                  className='flex flex-col items-center p-4 bg-slate-100 text-2xl rounded-lg shadow-md'
-                >
-                  <input
-                    {...props}
-                    className='outline-none bg-slate-100'
-                  />
-                </div>}
-            />
-          </div>
-          <button
-            onClick={handleConfirmToken}
-            className='w-full bg-slate-900 text-white p-2 rounded-lg'>Confirm</button>
-        </div>
-      )}
+    <div>
+      <Suspense fallback={<Loader2Icon size='64' className='animate-spin' />}>
+      <ConfirmEmailPageContents />
+      </Suspense>
     </div>
   )
 }
--- next_auth_v5/src/app/(root)/reset-password/page.tsx
+++ next_auth_v5/src/app/(root)/reset-password/page.tsx
@@ -1,11 +1,14 @@
 import SignForm from '@/components/sign_form'
-import React from 'react'
+import { Loader2Icon } from 'lucide-react'
+import React, { Suspense } from 'react'
 
 export default function Page() {
   return (
     <div className='w-full flex flex-col justify-center items-center bg-white p-4 rounded-lg max-w-screen-sm'>
-      <h1 className='text-lg font-bold'>Enter password to update</h1>
-      <SignForm type={"reset-password"} />
+      <Suspense fallback={<Loader2Icon size='64' className='animate-spin' />}>
+        <h1 className='text-lg font-bold'>Enter password to update</h1>
+        <SignForm type={"reset-password"} />
+      </Suspense>
     </div>
   )
 }
